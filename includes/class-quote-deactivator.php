89a8aeeda33e40fb90bb9c51f223d2eb8c5210a3
Reinhold Dreband <reinhold.deb@zoho.com>
2017-09-06 06:09:52 +0200

    quotePlugin for Wordpress

---
+++ includes/class-quote-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://
+ * @since      1.0.0
+ *
+ * @package    Quote
+ * @subpackage Quote/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Quote
+ * @subpackage Quote/includes
+ * @author     Rein.dre <reinhold.deb@zoho.com>
+ */
+class Quote_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
